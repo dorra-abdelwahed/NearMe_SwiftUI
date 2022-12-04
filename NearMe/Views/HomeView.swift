73991432cb55aea10bcf04dea95659dde7cf1a89
dabdelwahed <dabdelwahed@mobelite.fr>
2022-12-04 22:04:33 +0100

    set MVVM architecture

---
+++ NearMe/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NearMe
+//
+//  Created by Dorra Ben Abdelwahed on 4/12/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
