e107c92b50f233e8f3b5b4bc0adf559a3bb83e7b
dabdelwahed <dabdelwahed@mobelite.fr>
2022-12-04 21:20:02 +0100

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Dorra Ben Abdelwahed on 4/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
